8a8e57080b6ea8a68c3bb6d564fbce77eee5b6f4
Vincent Cheng <hsiangwen.cheng8@gmail.com>
2021-08-19 22:54:50 -0600

    Initial Commit

---
+++ calc.py
@@ -0,0 +1,74 @@
+import sys
+
+# handle add for multiple args
+# for example add 1 2 3 4 5 6
+# number of args can be empty(ignored)
+def additionHandler(func):
+    res = 0
+    try:
+        for index in range(1, len(func)):
+            res += int(func[index])
+    except ValueError as error:
+        print(error.args)
+        raise
+    return res
+
+# handle multiply for multiple args
+# for example mutiply 2 3 4 5 6 7
+# number of args must be greater than 2
+def multiplyHandler(func):
+    if(len(func) < 3):
+        raise Exception("Not enough args supplied to multiply function")
+    res = 1
+    try:
+        for index in range(1, len(func)):
+            res *= int(func[index])
+    except ValueError as error:
+        print(error.args)
+        raise
+    return res
+
+# handle func for add and multiply
+# we can add other function later
+def handleFunc(func):
+    if func[0] == 'add':
+        return additionHandler(func)
+    elif func[0] == 'multiply':
+        return multiplyHandler(func)
+    else:
+        raise Exception("Error: Unsupported function")
+
+def calc(input):
+    operationsCount = input.count(')')
+    # if a simple number is entered
+    # EXPR = INTEGER
+    if operationsCount == 0:
+        try:
+            return int(input)
+        except ValueError as error:
+            print(error.args)
+            raise
+    res = 0
+    # There are some func left...
+    while operationsCount > 0:
+        # get bounds
+        right = input.index(")")
+        left = input.rindex("(")
+        # extract func
+        func = input[left+1 : right].split()
+        # handle all func
+        res = handleFunc(func)
+        # update input string
+        input = input[0 : left] + str(res) + input[right+1 : len(input)] 
+        operationsCount -= 1
+    return res
+
+def main():
+    if len(sys.argv) == 2:
+        print(calc(sys.argv[1]))
+    else:
+        print("USAGE: ./calc.py FUNC")
+
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,61 @@
+import unittest
+
+from calc import calc
+
+
+class testCalc(unittest.TestCase):
+    def testNoFunc(self):
+        self.assertEqual(calc("123"), 123)
+
+    def testAdd(self):
+        self.assertEqual(calc("(add 123 456)"), 579)
+        self.assertEqual(calc("(add 1 1)"), 2)
+
+    def testAddAdd(self):
+        self.assertEqual(calc("(add 0 (add 3 4))"), 7)
+
+    def testAddAddAdd(self):
+        self.assertEqual(calc("(add 3 (add (add 3 3) 3))"), 12)
+
+    def testMultiply(self):
+        self.assertEqual(calc("(multiply 1 1)"), 1)
+
+    def testMultiplyMultiply(self):
+        self.assertEqual(calc("(multiply 0 (multiply 3 4))"), 0)
+        self.assertEqual(calc("(multiply 2 (multiply 3 4))"), 24)
+
+    def testMultiplyMultiplyMultiply(self):
+        self.assertEqual(calc("(multiply 3 (multiply (multiply 3 3) 3))"), 81)
+
+    def testMultiplyAdd(self):
+        self.assertEqual(calc("(multiply (add 1 2) 3)"), 9)
+
+    def testAddMultiply(self):
+        self.assertEqual(calc("(add 1 (multiply 2 3))"), 7)
+
+    def testMultiplyAddMultiply(self):
+        self.assertEqual(calc("(multiply 2 (add (multiply 2 3) 8))"), 28)
+
+    def testMultipleAddMultiply(self):
+        self.assertEqual(calc("(add 1 2 3 4 (multiply 2 3 5))"), 40)
+    
+    def testAddMultipleAdd(self):
+        self.assertEqual(calc("(add 1 (multiply (add 2 1) 3))"), 10)
+
+    def testInvalidFuncArgs(self):
+        with self.assertRaises(ValueError):
+            calc("(add 1 (multiply (add 2 1) z))")
+
+    def testInvalidFuncName(self):
+        with self.assertRaises(Exception):
+            calc("(Magic 1 (multiply (add 2 1) 1))")
+
+    def testInvalidMultiply(self):
+        with self.assertRaises(Exception):
+            calc("(Add 1 (multiply )")
+            calc("(Add 1 (multiply 1)")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
